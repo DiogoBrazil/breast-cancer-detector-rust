7225cb2c6d0968fb03899d4fdeb5ae8797881825
Diogo Ribeiro <diogoifroads@gmail.com>
2026-04-09 16:12:38 -0400

    Update output dimensions in SimpleDetector documentation

--- src/model/detector.rs
+++ src/model/detector.rs
@@ -7,7 +7,7 @@ use super::head::{DetectionHead, DetectionOutput};
 /// Detector anchor-free: backbone convolucional + detection head (cls + reg).
 ///
 /// Input:  imagens [B, 3, 416, 416]
-/// Output: cls_logits [B, 1, 13, 13] + reg_pred [B, 4, 13, 13]
+/// Output: cls_logits [B, 1, 26, 26] + reg_pred [B, 4, 26, 26]
 #[derive(Module, Debug)]
 pub struct SimpleDetector<B: Backend> {
     backbone: Backbone<B>,
