59fdcd00b40920e1c8bc90a2b3381ad44ad20964
Diogo Ribeiro <diogoifroads@gmail.com>
2026-04-10 06:12:53 -0400

    Refactor detector implementation to use ResNet-18 backbone and update documentation

--- src/model/detector.rs
+++ src/model/detector.rs
@@ -1,22 +1,22 @@
 use burn::module::Module;
 use burn::tensor::{Tensor, backend::Backend};
 
-use super::backbone::Backbone;
 use super::head::{DetectionHead, DetectionOutput};
+use super::resnet::ResNetBackbone;
 
-/// Detector anchor-free: backbone convolucional + detection head (cls + reg).
+/// Detector anchor-free: ResNet-18 backbone (até layer3) + detection head (cls + reg).
 ///
-/// Input:  imagens [B, 3, 416, 416]
+/// Input:  imagens [B, 3, 416, 416] (pixels [0, 1])
 /// Output: cls_logits [B, 1, 26, 26] + reg_pred [B, 4, 26, 26]
 #[derive(Module, Debug)]
-pub struct SimpleDetector<B: Backend> {
-    backbone: Backbone<B>,
-    head: DetectionHead<B>,
+pub struct Detector<B: Backend> {
+    pub backbone: ResNetBackbone<B>,
+    pub head: DetectionHead<B>,
 }
 
-impl<B: Backend> SimpleDetector<B> {
+impl<B: Backend> Detector<B> {
     pub fn new(device: &B::Device, num_classes: usize) -> Self {
-        let backbone = Backbone::new(device);
+        let backbone = ResNetBackbone::new(device);
         let head = DetectionHead::new(device, backbone.out_channels(), num_classes);
         Self { backbone, head }
     }
